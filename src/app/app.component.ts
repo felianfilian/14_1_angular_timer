970f20e13c8af43e6d9e2c4a51cec95dfd581e1e
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-10-04 20:43:15 +0200

    setup timer with rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, interval, throttle } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = '14_1_angular_timer';
+export class AppComponent implements OnInit {
+  timer = new BehaviorSubject<number>(0);
+
+  ngOnInit() {
+    this.timer
+      .pipe(throttle((val) => interval(2000)))
+      .subscribe((timePassed) => {
+        console.log(timePassed);
+      });
+
+    setInterval(() => {
+      let newValue = this.timer.value + 1000;
+      this.timer.next(newValue);
+    }, 100);
+  }
 }
